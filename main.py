d53dc35ab3bb5d1db011d2de0fd75820b988458b
Arun Rao Balappa <arunraobalappa@gmail.com>
2025-07-14 03:21:12 +0000

    container class used

--- main.py
+++ main.py
@@ -8,8 +8,13 @@ from player import *
 def main():
     pygame.init()
     clock = pygame.time.Clock()
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
     player = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
     dt = 0
+    
+    
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
@@ -19,10 +24,13 @@ def main():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
-        player.draw(screen)
-        player.update(dt)
+        for sprite in drawable:
+            sprite.draw(screen)
+        updatable.update(dt)    
         pygame.display.flip()
-        dt = clock.tick(60) / 1000
+        dt = clock.tick(60) / 1000 #it does 1000/60 first to get "ms" value
+                                   #then divide by 1000 to get "s" value
+                                   #also that 60 is a ceiling and not a guarantee
 
 
 
